54ad1ce0102b1706a63c56ae9df4a0a4de986fe0
Myat Thu Phone <phonemyatt.anewtech@gmail.com>
2018-02-07 21:43:32 +0800

    update chart

--- public/client.js
+++ public/client.js
@@ -1,16 +1,58 @@
-const socket = io();
+var socket = io();
 
-function getData() {    
-    const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {        
+
+var config = {
+    type: 'line',
+    data: {
+      labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+      datasets: [{
+        label: 'Voltage',
+        data: [12, 19, 3, 17, 6, 3, 7],
+        backgroundColor: "rgba(153,255,51,0.4)"
+      }, {
+        label: 'Current',
+        data: [2, 29, 5, 5, 2, 3, 10],
+        backgroundColor: "rgba(255,153,0,0.4)"
+      }]
+    }
+};
+
+window.onload = function () {
+    var ctx = document.getElementById('canvas').getContext('2d');
+    window.myLine = new Chart(ctx, config);
+};
+
+
+function getData() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             console.log(this.responseText);
         }
     };
     xhttp.open('GET', 'data', true);
-    xhttp.send();    
+    xhttp.send();
 }
 
-socket.on('message', function(socket){
-    console.log(socket);
-})
+function getIOLink() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            // console.log(this.responseText);
+            document.getElementById("devicetext").innerText=this.responseText;
+        }
+    };
+    xhttp.open('GET', 'iolink', true);
+    xhttp.send();
+}
+
+
+
+socket.on('message', function (data) {
+    console.log(data);
+});
+
+function setData(data) {
+    
+}
+
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const io = require('socket.io')(http);
 const path = require('path');
 const request = require('request');
 const port = process.env.PORT || 3000;
+const _ = require('lodash');
 
 app.use(express.static(__dirname + '/node_modules'));
 app.use('/public', express.static(__dirname + '/public'));
@@ -13,16 +14,58 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.get('/data', (req,res)=> {
+app.get('/data', (req, res)=> {
     request.get('https://jsonplaceholder.typicode.com/users')
     .pipe(res);
 });
 
-io.on('connection', (socket) => {
-    console.log('a user is connected');
-    setInterval(function(){
-        io.sockets.emit('message', 'hi');
-    }, 1000);
+app.get('/iolink', (req, res) => {
+    // request.get('http://192.168.0.2/dprop.jsn').pipe(res);
+
+    request('http://192.168.0.2/dprop.jsn', function (error, response, body) {
+        if ( response.statusCode == 200) {
+            // console.log( body );       
+            var temp = JSON.parse(body);
+            // console.log( temp[0].ProductId );
+
+            var myproduct = _.find(temp, function(o) {
+                return o.ProductId === "BAE00T4";
+            });
+            // console.log(myproduct);
+            res.send(myproduct.ProductText);
+        }
+    });
 });
 
-http.listen(port, () => console.log("server is started"));
+io.on('connection', (socket) => {
+    // console.log('a user is connected');
+    // setInterval(function(){
+    //     io.sockets.emit('message', 'hi');
+    // }, 1000);
+
+    setInterval(function() {
+        request('http://192.168.0.2/dprop.jsn', function(error,response,body) {
+            if ( response.statusCode == 200 ) {
+                var data = JSON.parse(body);
+                data = _.find(data, function(o) {
+                    return o.ProductId === "BAE00T4";
+                });
+                var processdata = data.ProcessInputs.split(' ').join('').substring(0,6);
+                console.log(processdata.substring(0,6));
+                var result = {                    
+                    "Voltage" : (parseInt(processdata.substr(0,3), 16) * 0.1).toString() ,
+                    "Current" : (parseInt(processdata.substring(3,6), 16) * 0.1).toString()
+                };
+
+                io.sockets.emit('message', result);
+            }
+        });
+    }, 1500);
+    
+});
+
+
+
+http.listen(port, function(){
+    console.log("server is started");
+});
